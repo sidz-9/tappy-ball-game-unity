754d7ef78de5e6a30430d098581e2f9aaadb3c85
sidz-9 <sid.zaveri.dev@gmail.com>
2021-02-09 03:02:36 +0530

    Added pipes and PipeController script, to control pipe movement and switch its speed/upDownSpeed.

---
+++ Assets/Scripts/PipeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed;
+    public float upDownSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Move();
+        InvokeRepeating("SwitchUpDownd", 0.1f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SwitchUpDownd() {
+        upDownSpeed = -upDownSpeed;
+        rb.velocity = new Vector2(-speed, upDownSpeed);
+    }
+
+    void Move() {
+        rb.velocity = new Vector2(-speed, upDownSpeed);
+    }
+}
